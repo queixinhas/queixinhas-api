78482eb5838560ec4793f6a72fff7c9cbdbece7c
jdduarte <jdduarte@gmail.com>
2012-11-21 18:37:19 +0000

    Changed identation to 2 spaces

--- test/db.js
+++ test/db.js
@@ -5,24 +5,23 @@ var should = require('should'),
 queixinhas_db.db_name = 'queixinhas_tests';
 
 describe('MongoDB accesser', function(){
-	it('should save, load and remove a document in the database', function(done){
-		var value = Date.now();
+  it('should save, load and remove a document in the database', function(done){
+    var value = Date.now();
 
-		queixinhas_db.save('test', {value: value}, function(error, id){
-			
-			queixinhas_db.load('test', id, function(error, docs){
-				docs[0].value.should.equal(value);
-				
-				queixinhas_db.remove('test', id, function(error, docs){
-					
-					queixinhas_db.load('test', id, function(error, docs){
-						docs.length.should.equal(0);
-						
-						done();
-					})
-				});
-			});
-		});
-	});
-	
+    queixinhas_db.save('test', {value: value}, function(error, id){
+
+      queixinhas_db.load('test', id, function(error, docs){
+        docs[0].value.should.equal(value);
+
+        queixinhas_db.remove('test', id, function(error, docs){
+
+          queixinhas_db.load('test', id, function(error, docs){
+            docs.length.should.equal(0);
+
+            done();
+          })
+        });
+      });
+    });
+  });	
 });
--- test/models.js
+++ test/models.js
@@ -2,45 +2,45 @@ var should = require('should'),
     models = require('../lib/models');
 
 describe('Models', function(){
-    describe('Report', function(){
+  describe('Report', function(){
 
-        var Report = models.Report;
+    var Report = models.Report;
 
-        it('should be synched with the DB', function(done){
-            var report = new Report({
-                name: 'report',
-                description: 'a report',
-                tags: ['tag#1', 'tag#2'],
-                location: {lon: -9, lat: 39},
-                address: 'in the sea'
-            });
+    it('should be synched with the DB', function(done){
+      var report = new Report({
+        name: 'report',
+        description: 'a report',
+        tags: ['tag#1', 'tag#2'],
+        location: {lon: -9, lat: 39},
+        address: 'in the sea'
+      });
 
-            // Change the default BD configs
-            report.queixinhas_db.db_name = 'queixinhas_tests';
+      // Change the default BD configs
+      report.queixinhas_db.db_name = 'queixinhas_tests';
 
-            report.on('save', function(){
-                report = new Report({
-                    id: report.id
-                });
+      report.on('save', function(){
+        report = new Report({
+          id: report.id
+        });
 
-                report.on('load', function(){
-                    report.name.should.equal('report');
-                    report.description.should.equal('a report');
-                    report.tags.should.eql(['tag#1', 'tag#2']);
-                    report.location.should.eql({lon: -9, lat: 39});
-                    report.address.should.equal('in the sea');
+        report.on('load', function(){
+          report.name.should.equal('report');
+          report.description.should.equal('a report');
+          report.tags.should.eql(['tag#1', 'tag#2']);
+          report.location.should.eql({lon: -9, lat: 39});
+          report.address.should.equal('in the sea');
 
-                    report.on('delete', function(){
-                        done();
-                    });
+          report.on('delete', function(){
+            done();
+          });
 
-                    report.delete();
-                });
+          report.delete();
+        });
 
-                report.load();
-            });
+        report.load();
+      });
 
-            report.save();
-        });    
-    });	
+      report.save();
+    });    
+  });	
 });
