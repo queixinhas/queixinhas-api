2d09d0668b59212045226a47450dbd2fe4f99000
Joao Duarte <jdduarte@gmail.com>
2013-01-16 23:59:28 +0000

    Remove save, load and remove methods. Update tests

--- test/models.js
+++ test/models.js
@@ -4,45 +4,5 @@ var should = require('should'),
 describe('Models', function(){
   describe('Report', function(){
 
-    var Report = models.Report;
-
-    it('should be synched with the DB', function(done){
-      var report = new Report({
-        name: 'report',
-        description: 'a report',
-        tags: ['tag#1', 'tag#2'],
-        location: {lon: -9, lat: 39},
-        address: 'in the sea',
-        mediaItems: ['http://image.com']
-      });
-
-      // Change the default BD configs
-      report.queixinhas_db.db_name = 'queixinhas_tests';
-
-      report.on('save', function(){
-        report = new Report({
-          id: report.id
-        });
-
-        report.on('load', function(){
-          report.name.should.equal('report');
-          report.description.should.equal('a report');
-          report.tags.should.eql(['tag#1', 'tag#2']);
-          report.location.should.eql({lon: -9, lat: 39});
-          report.address.should.equal('in the sea');
-          report.mediaItems.should.eql(['http://image.com']);
-
-          report.on('delete', function(){
-            done();
-          });
-
-          report.delete();
-        });
-
-        report.load();
-      });
-
-      report.save();
-    });    
   });	
 });
